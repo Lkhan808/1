bb3368321d5b592bcb27b509450b2d83d6e6eb5d
Эльхан <pirigamelkhan@icloud.com>
2023-02-24 19:52:55 +0600

    first commit

--- HW7.py
+++ HW7.py
@@ -14,10 +14,27 @@ def bubbleSort(array):
                 array[j] = array[j + 1]
                 array[j + 1] = temp
 
-data = [-2, 45, 0, 11, -9]
+#data = [-2, 45, 0, 11, -9]
 
-bubbleSort(data)
+#bubbleSort(data)
 
-print('Sorted Array in Ascending Order:')
-print(data)
+#print('Sorted Array:')
+#print(data)
+
+def binarySearch(array, x, low, high):
+    while low <= high:
+        mid = low + (high - low)//2
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return x
+#array = [3, 4, 5, 6, 7, 8, 9]
+#x = 7
+#print(binarySearch(array, x, 0, len(array)-1))
+
+
+
 
